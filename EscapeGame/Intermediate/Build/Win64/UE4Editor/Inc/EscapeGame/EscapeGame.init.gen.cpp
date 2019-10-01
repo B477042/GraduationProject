06d70531c8bd450d180fe1e400318062f5deb01e
B477042 <37619106+B477042@users.noreply.github.com>
2019-10-01 19:13:13 +0900

    2019 10 01 벡압
    ㄴㅁㄹ

--- EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGame.init.gen.cpp
+++ EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGame.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeEscapeGame_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/EscapeGame",
 				PKG_CompiledIn | 0x00000000,
-				0x7EADB15B,
-				0xBA4D77BD,
+				0x49D7C426,
+				0xA36BB866,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
---
+++ EscapeGame/Source/EscapeGame/public/EGPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EscapeGame.h"
+#include "GameFramework/PlayerController.h"
+#include "EGPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEGAME_API AEGPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
