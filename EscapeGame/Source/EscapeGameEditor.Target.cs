c0ed023bac8c01eb0171f283ad755fee7602bedf
B477042 <37619106+B477042@users.noreply.github.com>
2019-11-10 17:53:03 +0900

    Add Stucture Module
    추가만

--- EscapeGame/Source/EscapeGameEditor.Target.cs
+++ EscapeGame/Source/EscapeGameEditor.Target.cs
@@ -9,6 +9,6 @@ public class EscapeGameEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 
-		ExtraModuleNames.AddRange( new string[] { "EscapeGame","GameSetting" } );
+		ExtraModuleNames.AddRange( new string[] { "EscapeGame","GameSetting", "Structure" } );
 	}
 }
