efaa81a392355f43ccc2806f9762e40659d4bd17
B477042 <37619106+B477042@users.noreply.github.com>
2019-12-26 21:01:06 +0900

    fire ball trap 세부 조정

--- EscapeGame/Source/EscapeGame/private/FireBallTrap.cpp
+++ EscapeGame/Source/EscapeGame/private/FireBallTrap.cpp
@@ -68,6 +68,7 @@ void AFireBallTrap::Fire()
 	FireBall->Fire();
 	Trigger->SetCollisionProfileName(TEXT("NoCollision"));
 	FireBall.Get();
+	Destroy();
 }
 
 void AFireBallTrap::OnPlayerEntered(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
