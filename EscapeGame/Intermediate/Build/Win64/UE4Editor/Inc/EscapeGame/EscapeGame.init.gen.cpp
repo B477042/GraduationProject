493d3cef5245a6683999413aaab725f77750ca58
B477042 <37619106+B477042@users.noreply.github.com>
2020-02-18 13:18:32 +0900

    문제 : stat comp Enemy에서 발생.
    Stat comp 정리할 것

--- EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGame.init.gen.cpp
+++ EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGame.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeEscapeGame_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB9E4C442,
-				0xE73EF459,
+				0x0873B0D5,
+				0x89AA13E4,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
