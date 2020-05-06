b2e250d66825fe0ad6b7a60fcd5d791368ee80cb
B477042 <37619106+B477042@users.noreply.github.com>
2020-05-07 00:51:55 +0900

    back up

--- EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGame.init.gen.cpp
+++ EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGame.init.gen.cpp
@@ -31,8 +31,8 @@ void EmptyLinkFunctionForGeneratedCodeEscapeGame_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x89C4CF51,
-				0xBA814446,
+				0x8D9C3395,
+				0x0A2A4099,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
---
+++ EscapeGame/Source/EscapeGame/private/Component_Inventory.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Component_Inventory.h"
+
+// Sets default values for this component's properties
+UComponent_Inventory::UComponent_Inventory()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UComponent_Inventory::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UComponent_Inventory::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
