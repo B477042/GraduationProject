5904b478bbcab15d47e66e8e8b3b037cd480f2ce
B477042 <37619106+B477042@users.noreply.github.com>
2020-05-15 08:40:08 +0900

    back up

--- EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGame.init.gen.cpp
+++ EscapeGame/Intermediate/Build/Win64/UE4Editor/Inc/EscapeGame/EscapeGame.init.gen.cpp
@@ -35,8 +35,8 @@ void EmptyLinkFunctionForGeneratedCodeEscapeGame_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x50216B6E,
-				0x585F8469,
+				0xF1B1B496,
+				0x1567ADBF,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
--- EscapeGame/Source/EscapeGame/public/Weapon.h
+++ EscapeGame/Source/EscapeGame/public/Weapon.h
@@ -2,11 +2,17 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "EscapeGame.h"
 #include "GameFramework/Actor.h"
 #include "Weapon.generated.h"
 
-UCLASS()
+/*
+
+	BP에서 상속으로 무기를 제작해주는게 좋을거 같다
+
+*/
+
+UCLASS(Blueprintable)
 class ESCAPEGAME_API AWeapon : public AActor
 {
 	GENERATED_BODY()
@@ -18,9 +24,39 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void PostInitializeComponents()override;
+
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	float GetDamage();
+
+	UFUNCTION()
+	void EquipTo(ACharacter* OtherActor);
+
+
+private:
+	void initComponents();
+
+	UFUNCTION(BlueprintCallable)
+		void OnActorBeginOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	//Init Body(Component) Collision Profile Name
+	UFUNCTION(BlueprintCallable)
+		void initBodyCollision();
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+		UStaticMeshComponent* Body;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+		float Damage;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+		UParticleSystemComponent* Effect;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		ACharacter* OwnerChara;
+	//도신을 덮는다
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+		UBoxComponent* AttackRangeBox;
 };
