085380c38052675128fb35c7c80af68994ee24a2
B477042 <37619106+B477042@users.noreply.github.com>
2020-05-15 09:35:04 +0900

    sdaf

--- EscapeGame/Source/EscapeGame/private/Weapon.cpp
+++ EscapeGame/Source/EscapeGame/private/Weapon.cpp
@@ -2,12 +2,15 @@
 
 
 #include "Weapon.h"
+#include "EGPlayerCharacter.h"
 
 // Sets default values
 AWeapon::AWeapon()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	initComponents();
 
 }
 
@@ -16,12 +19,63 @@ void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	Effect->bAutoActivate = false;
+
 }
+void AWeapon::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	
+	AttackRangeBox->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnActorBeginOverlap);
+
+}
+
 
 // Called every frame
 void AWeapon::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+}
+
+float AWeapon::GetDamage()
+{
+	return Damage;
+}
+
+void AWeapon::EquipTo(ACharacter * OtherActor)
+{
+	OwnerChara = OtherActor;
+}
+
+void AWeapon::initComponents()
+{
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BODY"));
+	Effect = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Effect"));
+	AttackRangeBox = CreateDefaultSubobject<UBoxComponent>(TEXT("BOXCOMP"));
+	RootComponent = AttackRangeBox;
+	Effect->SetupAttachment(RootComponent);
+	Body->SetupAttachment(RootComponent);
+	Damage = 10;
+
+}
+
+void AWeapon::OnActorBeginOverlap(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	//EGLOG(Error, TEXT("Hi Master"));
+	auto player = Cast<AEGPlayerCharacter>(OtherActor);
+	if (!player) { return; }
+	Effect->Activate();
+	
+}
+
+
+void AWeapon::initBodyCollision()
+{
+	if (!Body->GetStaticMesh())return;
+
+	Body->SetCollisionProfileName(TEXT("PlayerWeapon"));
+	
 
 }
 
---
+++ EscapeGame/Source/EscapeGame/public/StateComponent_Player.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StateComponent.h"
+#include "StateComponent_Player.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEGAME_API UStateComponent_Player : public UStateComponent
+{
+	GENERATED_BODY()
+	
+};
